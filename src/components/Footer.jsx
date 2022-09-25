1dea92c0a3f63a2b6ea434c48d44ae029ed27673
UgurcanSARICI <ugurcansarici@gmail.com>
2022-09-25 23:31:05 +0300

    footer/ contact us

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center py-2 flex-col">
+      <p className="font-semibold">All Rights Reserved © 2022</p>
+      <p className="">Coded By Alpha Tech</p>
+    </div>
+  );
+};
+
+export default Footer;
